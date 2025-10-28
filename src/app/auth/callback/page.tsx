a1e634083bf719b06acfa8d9a600c599a1855d90
razeth <seth.razeth@gmail.com>
2025-10-28 17:43:15 +0700

    temporary disabled callback page

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -12,17 +12,17 @@
 // }
 
 // app/auth/callback/page.tsx
-import AuthCallbackPage from "./AuthCallbackPage";
+// import AuthCallbackPage from "./AuthCallbackPage";
 
-export default async function Page({
-  searchParams,
-}: {
-  // searchParams?: { [key: string]: string | string[] | undefined };
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-}) {
-  // Await the promise that Next.js passes in
-  const resolved = await searchParams;
+// export default async function Page({
+//   searchParams,
+// }: {
+//   // searchParams?: { [key: string]: string | string[] | undefined };
+//   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+// }) {
+//   // Await the promise that Next.js passes in
+//   const resolved = await searchParams;
 
-  return <AuthCallbackPage searchParams={resolved} />;
-  // return <AuthCallbackPage searchParams={searchParams ?? {}} />;
-}
+//   return <AuthCallbackPage searchParams={resolved} />;
+//   // return <AuthCallbackPage searchParams={searchParams ?? {}} />;
+// }
