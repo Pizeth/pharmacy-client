1a3f7b5bc1e0ba9a320d20861ee34a5eccf9c77a
Razeth <seth.razeth@gmail.com>
2026-09-26 08:15:41 +0700

    feat(datatable): add Refine adapter foundation

---
+++ src/components/DataTable/adapters/refine/index.ts
@@ -0,0 +1,20 @@
+export {
+  createRefineDataTableQueryAdapter,
+  createRefineOrContainsSearchFilters,
+} from "./createRefineDataTableQueryAdapter";
+
+export {
+  adaptRefineGetListResponse,
+} from "./adaptRefineGetListResponse";
+
+export {
+  createRefineDataTableAdapter,
+} from "./createRefineDataTableAdapter";
+
+export type {
+  CreateRefineDataTableAdapterOptions,
+  CreateRefineDataTableQueryAdapterOptions,
+  RefineDataTableAdapter,
+  RefineDataTableSearchFilterMapper,
+  RefineDataTableUnsupportedSearchPolicy,
+} from "./types";
